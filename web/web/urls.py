a0722c2bd8326822100c9c445c16d3e598e17bde
mruwzum <miganber@alum.us.es>
2019-02-03 15:07:03 +0100

    cxv

--- web/web/urls.py
+++ web/web/urls.py
@@ -20,7 +20,3 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('igmeter.urls')),
 ]
-for module in settings.MODULES:
-    urlpatterns += [
-        path('{}/'.format(module), include('{}.urls'.format(module)))
-    ]
